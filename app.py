62dc94a20a89a8c2b0300b24ddf528210993a952
k-sugawa <sugawakumiko@gmail.com>
2024-07-09 22:05:23 +0900

    pandasでcsvを読み込みhtml出力

---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import time
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+url = './xlsx/sample.csv'
+
+def load_csv_data():
+    start = time.time()
+    # 列指定usecols
+    # データタイプ文字列指定 dtype=str
+    df = pd.read_csv(url, header=0, usecols=[0, 3, 7, 8], dtype=str, na_values='-')
+    end = time.time()
+    print("CSVの読み込み時間:", end - start)
+    print(df)
+    return df
+
+@app.route('/', methods=("POST", "GET"))
+def index():
+    df_data = load_csv_data()
+    return render_template('index.html', tables=[df_data.to_html(classes='data')], titles=df_data.columns.values)
+
+if __name__ == '__main__':
+    app.run(debug=True)
